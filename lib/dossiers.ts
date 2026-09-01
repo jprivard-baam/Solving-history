2c0029201ed58d191af0794419556aabd89cb1ea
jprivard <jprivard@live.ca>
2026-09-01 20:00:29 +0000

    Drop Carto API-key watermark: OSM raster, no key

--- lib/dossiers.ts
+++ lib/dossiers.ts
@@ -83,7 +83,7 @@ export function getDossier(id: string): DossierMeta | undefined {
 }
 
 export const CARTO_DARK_URL =
-  "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png";
+  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
 export const CARTO_ATTRIBUTION =
-  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>';
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>';
